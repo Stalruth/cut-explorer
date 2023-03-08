81047219deaea68dce262b4dd9db80e11fdd3bf1
Stuart Allan Rutherford <stalruth@gmail.com>
2023-03-08 22:12:28 +1100

    Add acknowledgements

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,6 +8,6 @@ import '$lib/css/app.css';
 </main>
 <footer>
   <p>
-    Site by <a href="https://stalruth.dev">Stuart Rutherford</a>.
+    Site by <a href="https://stalruth.dev">Stuart Rutherford</a>.  Team exports provided by <a href="https://twitter.com/VGCPastes">VGC Pastes</a>.  Special thanks to <a href="https://github.com/pkmn/ps">pkmn/ps</a> and apologies to <a href="https://pokepast.es">PokePastes</a>.
   </p>
 </footer>
