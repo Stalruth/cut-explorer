8179f19c4eab9faa5227e8c1a1f4bff11a1e6847
Stuart Allan Rutherford <stalruth@gmail.com>
2023-03-18 23:01:58 +1100

    Add more acknowledgements

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,6 +8,6 @@ import '$lib/css/app.css';
 </main>
 <footer>
   <p>
-    Site by <a href="https://stalruth.dev">Stuart Rutherford</a>.  Team exports provided by <a href="https://twitter.com/VGCPastes">VGC Pastes</a>.  Special thanks to <a href="https://github.com/pkmn/ps">pkmn/ps</a> and apologies to <a href="https://pokepast.es">PokePastes</a>.
+    Site by <a href="https://stalruth.dev">Stuart Rutherford</a>.  Team exports and standings provided by <a href="https://rk9.gg/">RK9 Labs</a> via <a href="https://twitter.com/VGCPastes">VGC Pastes</a> and <a href="https://pokedata.ovh/">PokeData</a>.  Special thanks to <a href="https://github.com/pkmn/ps">pkmn/ps</a> and apologies to <a href="https://pokepast.es/">PokePaste</a>.
   </p>
 </footer>
