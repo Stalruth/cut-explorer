af72d28d48e55794d57b0223bab6b231ca44a27d
Stuart Allan Rutherford <stalruth@gmail.com>
2024-07-29 23:32:56 +1000

    Update dependencies + Netlify Adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import * as child_process from 'node:child_process';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
