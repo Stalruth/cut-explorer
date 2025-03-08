f17c97da46e6ce1db7b40c11a64730a719197523
Stuart Allan Rutherford <stalruth@gmail.com>
2025-03-08 17:55:09 +1100

    Kill all old caches

--- src/service-worker.js
+++ src/service-worker.js
@@ -5,3 +5,14 @@ pageCache({warmCache: ['/', ...build.filter(el => el.includes('2025'))]});
 staticResourceCache({warmCache: files.filter(el => el.endsWith('css') || el.endsWith('js') || (el.endsWith('json') && el.includes('2025')))});
 imageCache({warmCache: files.filter(el => el.endsWith('png'))});
 
+self.addEventListener('activate', e => {
+  // prune old caches
+    async function pruneCaches() {
+    for (const key of await caches.keys()) {
+      if (!['pages', 'static-resources', 'images'].includes(key)) await caches.delete(key);
+    }
+  }
+
+  e.waitUntil(pruneCaches());
+});
+
