f32916375fe3054dbdc54e2d033b0dc770f7f5f6
Stuart Allan Rutherford <stalruth@gmail.com>
2025-03-09 22:32:03 +1100

    Add fetches to the Static Resource Cache

--- src/service-worker.js
+++ src/service-worker.js
@@ -2,7 +2,14 @@ import { build, files, version } from '$service-worker';
 import { pageCache, imageCache, staticResourceCache } from 'workbox-recipes';
 
 pageCache({warmCache: ['/', ...build.filter(el => el.includes('2025'))]});
-staticResourceCache({warmCache: files.filter(el => el.endsWith('css') || el.endsWith('js') || (el.endsWith('json') && el.includes('2025')))});
+
+const warmCache = files.filter(el => el.endsWith('css') || el.endsWith('js') || el.endsWith('webmanifest') || (el.endsWith('json') && el.includes('2025')));
+const matchCallback = ({ request }) => ['style', 'script', 'worker'].includes(request.destination) || !request.destination;
+staticResourceCache({
+  warmCache,
+  matchCallback
+});
+
 imageCache({warmCache: files.filter(el => el.endsWith('png'))});
 
 self.addEventListener('activate', e => {
