8d26ab0671c19f5e42ec0b670711b8d920ac1731
Stuart Allan Rutherford <stalruth@gmail.com>
2025-03-11 21:51:54 +1100

    Remove console.log from root data

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -4,7 +4,6 @@ export const load: Pageload = async ({ fetch, params }) => {
   const years = await (
     await fetch('/data/tournaments/years.json')
   ).json();
-  console.log(years,`/data/tournaments/${years[years.length - 1]}/tournaments.json`);
   const tourInfo = await (
     await fetch(`/data/tournaments/${years[years.length - 1]}/tournaments.json`)
   ).json();
