d923e73c3901967c79765127d2bf4f3474053775
Stuart Rutherford <squirrel96@live.co.uk>
2025-12-26 21:20:09 +1100

    Increase timeout

--- src/service-worker.js
+++ src/service-worker.js
@@ -13,7 +13,7 @@ offlineFallback(); // uses cache 'workbox-offline-fallbacks'
 const dataMatch = ({url}) => url.origin === API_SERVER;
 const dataStrategy = new NetworkFirst({
   cacheName: 'data',
-  networkTimeoutSeconds: 1
+  networkTimeoutSeconds: 2
 });
 registerRoute(dataMatch, dataStrategy);
 
