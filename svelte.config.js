2542250c9eb5bd6ef143c12affd4408e3bbc5bce
Stuart Rutherford <squirrel96@live.co.uk>
2026-05-21 15:16:20 +1000

    Update deps, use node adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import * as child_process from 'node:child_process';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,8 +8,7 @@ const config = {
 
   kit: {
     adapter: adapter({
-      fallback: 'offline.html',
-      pages: process.env.OUT_DIR
+      fallback: 'offline.html'
     }),
     version: {
       name: child_process.execSync('git rev-parse HEAD').toString().trim()
@@ -18,3 +17,4 @@ const config = {
 };
 
 export default config;
+
