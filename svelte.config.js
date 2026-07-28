8c0a4dbd912becf046f2fa1a3f97aa6f1a2ae88e
Stuart Rutherford <squirrel96@live.co.uk>
2026-07-28 18:29:08 +1000

    Add CSP directives

--- svelte.config.js
+++ svelte.config.js
@@ -12,6 +12,15 @@ const config = {
       pages: process.env.OUT_DIR
     }),
     csp: {
+      'base-uri': ['self'],
+      'connect-src': ['self'],
+      'default-src': ['self'],
+      'font-src': ['self'],
+      'form-action': ['self'],
+      'frame-ancestors': ['self'],
+      'script-src': ['self'],
+      'style-src': ['self'],
+      'unsafe-inline': ['img-src', 'self', 'data'],
       mode: 'auto'
     },
     version: {
